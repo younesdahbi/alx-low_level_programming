be48f3b0965f13703695b52d034da06d28aac3d0
YD21 <younes21dahbi@gmail.com>
2023-06-21 13:28:11 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,16 @@
-#include <stdlib.h>
+#include "main.h"
 
-#include <stdio.h>
+int main() {
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-
-        printf("_putchar\n");
-
-return (0);
+    return 0;
 }
+
