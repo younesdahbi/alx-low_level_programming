23785a31b20fe38d95c1b2a8a73cc6627eee781c
YD21 <younes21dahbi@gmail.com>
2023-06-21 13:33:10 -0700

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,24 @@
 #include "main.h"
 
-int main() {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the string "_putchar" followed by a newline character
+ *
+ * Return: Always 0 (indicating success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-    return 0;
+	return (0);
 }
 
