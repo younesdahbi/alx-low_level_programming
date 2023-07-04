42e67f21e817d7f6488f5a35cc6a2116be249645
YD21 <younes21dahbi@gmail.com>
2023-07-03 19:31:53 -0700

    00

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,7 +2,9 @@
 
 /**
  * print_chessboard - prints the chessboard
- * @a: two dimension array to print
+ * @a: pointer to pieces to print
+ *
+ * Return: void
  */
 void print_chessboard(char (*a)[8])
 {
