2d9b029b7378aab053c3e3e0ec2b31c109ae6fda
YD21 <younes21dahbi@gmail.com>
2023-07-07 20:02:17 -0700

    22222

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-
-# Compile all .c files into .o object files
 gcc -c *.c
-
-# Create the static library liball.a from the object files
-ar -rc liball.a *.o
-
-# Index the symbols in the library
+ar rc liball.a *.o
 ranlib liball.a
-
-# Clean up the temporary object files
-rm *.o
