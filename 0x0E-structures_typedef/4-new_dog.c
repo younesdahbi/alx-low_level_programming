32519a1bf1dd0632a6f2196a404c5ea46fe1afb5
YD21 <younes21dahbi@gmail.com>
2023-07-17 20:09:59 -0700

    00

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,16 +1,47 @@
+#include <stdlib.h>
 #include "dog.h"
-#include <stdio.h>
+
 /**
- * _strlen - a dog struct
- * @str: initial the dog
- * Return: void
-*/
-
-int _strlen(const char *str)
+ * new_dog - creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: pointer to new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
 {
-	int length = 0;
+unsigned int nl, ol, i;
+dog_t *dog;
 
-	while (*str++)
-		length++;
-	return (length);
+if (name == NULL || owner == NULL)
+return (NULL);
+dog = malloc(sizeof(dog_t));
+if (dog == NULL)
+return (NULL);
+for (nl = 0; name[nl]; nl++)
+;
+nl++;
+dog->name = malloc(nl * sizeof(char));
+if (dog->name == NULL)
+{
+free(dog);
+return (NULL);
+}
+for (i = 0; i < nl; i++)
+dog->name[i] = name[i];
+dog->age = age;
+for (ol = 0; owner[ol]; ol++)
+;
+ol++;
+dog->owner = malloc(ol * sizeof(char));
+if (dog->owner == NULL)
+{
+																					free(dog->name);
+free(dog);
+return (NULL);
+}
+for (i = 0; i < ol; i++)
+dog->owner[i] = owner[i];
+return (dog);
 }
