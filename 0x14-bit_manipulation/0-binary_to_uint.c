762361d4ed59c68195547f9345a60b5eb511add7
YD21 <younes21dahbi@gmail.com>
2023-08-03 19:46:37 -0700

    2

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,34 +1,25 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * char string
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
  *
- * convert a binary number to an unsigned int
- * 
- * converted decimal number or 0 if there is an unconvertable char
+ * Return: the converted number
  */
-
-
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int total, power;
-	int len;
+	int i;
+	unsigned int dec_val = 0;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 
-	for (len = 0; b[len]; len++)
+	for (i = 0; b[i]; i++)
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 
-	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
-	{
-		if (b[len] == '1')
-			total += power;
-	}
-
-	return (total);
+	return (dec_val);
 }
--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -1,14 +1,13 @@
+#include "main.h"
 #include <unistd.h>
-
-/*
- * Writes the character c to stdout
- * 
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: 1. on success and -1. on error.
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
 int _putchar(char c)
-
 {
 	return (write(1, &c, 1));
 }
